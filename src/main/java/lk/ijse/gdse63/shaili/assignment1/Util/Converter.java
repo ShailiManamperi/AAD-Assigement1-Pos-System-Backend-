e028440007ee587a36f76bda898fc3ed5db29741
Shaili Manamperi <shailimanamperi2002@gmail.com>
2023-07-29 11:28:04 +0530

    add converter

---
+++ src/main/java/lk/ijse/gdse63/shaili/assignment1/Util/Converter.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse63.shaili.assignment1.Util;
+
+import lk.ijse.gdse63.shaili.assignment1.DTO.ItemDTO;
+import lk.ijse.gdse63.shaili.assignment1.Entity.Item;
+
+public class Converter {
+    public ItemDTO fromItem(Item item){
+        return new ItemDTO(item.getId(), item.getName(), item.getQty(), item.getPrice());
+    }
+
+    public Item toItem(ItemDTO itemDTO){
+        return new Item(itemDTO.getId(), itemDTO.getName(), itemDTO.getQty(), itemDTO.getPrice());
+    }
+}
